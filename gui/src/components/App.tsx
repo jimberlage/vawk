4c6dd6496ee17564e1c6ce13051245ccfdf5ebcd
Jim Berlage <james.berlage@gmail.com>
2021-01-23 12:06:04 -0600

    Send request options

--- gui/src/components/App.tsx
+++ gui/src/components/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Button, Form, Input, Tabs } from 'antd';
 import LineOptionsForm from './LineOptionsForm';
 import { OutputMessage, addChunk, isComplete, combineStdoutChunks, combineStderrChunks } from '../parser';
+import { changeLineSeparators, changeRowSeparators } from './requests';
 import 'antd/dist/antd.css';
 
 class InvalidServerEventError extends Error {
@@ -15,20 +16,36 @@ type RowProps = {
   row: string[];
 };
 
+let initializeLineSeparators = async (
+  clientId: string, 
+  setLineSeparators: React.Dispatch<React.SetStateAction<string[] | undefined>>,
+  setError: React.Dispatch<React.SetStateAction<Error | undefined>>,
+) => {
+  await changeLineSeparators(clientId, undefined, ['\\n'], setLineSeparators, setError);
+};
+
+let initializeRowSeparators = async (
+  clientId: string, 
+  setRowSeparators: React.Dispatch<React.SetStateAction<string[] | undefined>>,
+  setError: React.Dispatch<React.SetStateAction<Error | undefined>>,
+) => {
+  await changeRowSeparators(clientId, undefined, ['\\n'], setRowSeparators, setError);
+};
+
 let Row = (props: RowProps) => {
   return (
-    <tr>
+    <tr className="whitespace-pre-wrap">
       {props.row.map((cell, index) => <td key={`${index}:${cell}`}>{cell}</td>)}
     </tr>
   );
 };
 
 type changeCommandFormValues = {
-  'client_id': String | undefined;
-  command: String;
+  'client_id': string | undefined;
+  command: string;
 }
 
-let changeCommand = (clientId: String, values: changeCommandFormValues) => {
+let changeCommand = (clientId: string, values: changeCommandFormValues) => {
   values['client_id'] = clientId;
   // TODO: Error handling
   fetch('http://localhost:6846/api/command/run', {
@@ -41,7 +58,7 @@ let changeCommand = (clientId: String, values: changeCommandFormValues) => {
 };
 
 type ChangeCommandFormProps = {
-  clientId: String;
+  clientId: string;
 }
 
 let ChangeCommandForm = ({ clientId }: ChangeCommandFormProps) => {
@@ -61,7 +78,7 @@ let ChangeCommandForm = ({ clientId }: ChangeCommandFormProps) => {
 
 let App = () => {
   // Initialize the app, resetting the time the server has last checked to ensure that it submits an update.
-  const [clientId, setClientId] = useState<String | undefined>();
+  const [clientId, setClientId] = useState<string | undefined>();
   const [initStatus, setInitStatus] = useState<'uninitialized' | 'pending' | 'initialized'>('uninitialized');
 
   useEffect(() => {
@@ -95,9 +112,12 @@ let App = () => {
   }, [initStatus, setInitStatus, setClientId]);
 
   // Our default IFS is a newline character, but that can be changed at the user level.
-  const [lineSeparators, setLineSeparators] = useState<string>('\\n');
-  const [lineRegex, setLineRegex] = useState<string>('');
-  const [lineIndices, setLineIndices] = useState<string>('');
+  const [lineSeparators, setLineSeparators] = useState<string[] | undefined>(['\\n']);
+  const [lineRegex, setLineRegex] = useState<string | undefined>('');
+  const [lineIndices, setLineIndices] = useState<string | undefined>('');
+  const [rowSeparators, setRowSeparators] = useState<string[] | undefined>(['\\s']);
+  const [rowRegex, setRowRegex] = useState<string | undefined>('');
+  const [rowIndices, setRowIndices] = useState<string | undefined>('');
 
   const [updateStream, setUpdateStream] = useState<EventSource | undefined>(undefined);
   // Manages our current line buffer.
@@ -117,42 +137,49 @@ let App = () => {
 
   // Listen for updates when the app is loaded (and cleanup after ourselves).
   useEffect(() => {
-    if (clientId && !updateStream) {
-      let source = new EventSource(`http://localhost:6846/api/listen?client_id=${clientId}`);
-      source.addEventListener('stdout', (event) => {
-        if (!(event as MessageEvent)?.data) {
-          setError(new InvalidServerEventError());
-          return
-        }
+    (async () => {
+      if (clientId && !updateStream) {
+        await Promise.all([
+          initializeLineSeparators(clientId, setLineSeparators, setError),
+          initializeRowSeparators(clientId, setRowSeparators, setError),
+        ]);
 
-        // TODO: Use correct error type (InvalidServerEventError)
-        let newStdoutMessage = addChunk((event as MessageEvent).data, stdoutMessage);
-        if (isComplete(newStdoutMessage)) {
-          setStdout(combineStdoutChunks(newStdoutMessage));
-          setStdoutMessage(undefined);
-        } else {
-          setStdoutMessage(newStdoutMessage);
-        }
-      });
+        let source = new EventSource(`http://localhost:6846/api/listen?client_id=${clientId}`);
+        source.addEventListener('stdout', (event) => {
+          if (!(event as MessageEvent)?.data) {
+            setError(new InvalidServerEventError());
+            return
+          }
 
-      source.addEventListener('stderr', (event) => {
-        if (!(event as MessageEvent)?.data) {
-          setError(new InvalidServerEventError());
-          return
-        }
+          // TODO: Use correct error type (InvalidServerEventError)
+          let newStdoutMessage = addChunk((event as MessageEvent).data, stdoutMessage);
+          if (isComplete(newStdoutMessage)) {
+            setStdout(combineStdoutChunks(newStdoutMessage));
+            setStdoutMessage(undefined);
+          } else {
+            setStdoutMessage(newStdoutMessage);
+          }
+        });
 
-        let newStderrMessage = addChunk((event as MessageEvent).data, stderrMessage);
-        if (isComplete(newStderrMessage)) {
-          setStderr(combineStderrChunks(newStderrMessage));
-          setStderrMessage(undefined);
-        } else {
-          setStderrMessage(newStderrMessage);
-        }
-      });
+        source.addEventListener('stderr', (event) => {
+          if (!(event as MessageEvent)?.data) {
+            setError(new InvalidServerEventError());
+            return
+          }
 
-      setUpdateStream(source);
-    }
-  }, [clientId, updateStream, stdoutMessage, stderrMessage, setUpdateStream, setStdoutMessage, setStderrMessage, setStdout, setStderr]);
+          let newStderrMessage = addChunk((event as MessageEvent).data, stderrMessage);
+          if (isComplete(newStderrMessage)) {
+            setStderr(combineStderrChunks(newStderrMessage));
+            setStderrMessage(undefined);
+          } else {
+            setStderrMessage(newStderrMessage);
+          }
+        });
+
+        setUpdateStream(source);
+      }
+    })()
+  }, [clientId, updateStream, stdoutMessage, stderrMessage, setUpdateStream, setStdoutMessage, setStderrMessage, setStdout, setStderr, setLineSeparators, setRowSeparators, setError]);
 
   return (
     <>
@@ -183,10 +210,16 @@ let App = () => {
         </main>
         <aside className="w-1/4">
           {clientId ? <ChangeCommandForm clientId={clientId} /> : null }
-          <LineOptionsForm separators={lineSeparators}
-                           setSeparators={setLineSeparators}
-                           setRegex={setLineRegex}
-                           setIndices={setLineIndices} />
+          {clientId ?
+            <LineOptionsForm clientId={clientId}
+                             indices={lineIndices}
+                             regex={lineRegex}
+                             separators={lineSeparators}
+                             setIndices={setLineIndices}
+                             setRegex={setLineRegex}
+                             setSeparators={setLineSeparators}
+                             setError={setError} />
+            : null}
         </aside>
       </section>
     </>
--- gui/src/components/LineOptionsForm.tsx
+++ gui/src/components/LineOptionsForm.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
 import { Input } from 'antd';
+import { changeLineIndices, changeLineRegex, changeLineSeparators } from './requests';
 
 type LineOptionsFormProps = {
-    setIndices: (indicesStr: string) => void;
-    separators: string,
-    setSeparators: (separatorsStr: string) => void;
-    setRegex: (regexStr: string) => void;
+    clientId: string,
+    indices: string | undefined,
+    regex: string | undefined,
+    separators: string[] | undefined,
+    setIndices: React.Dispatch<React.SetStateAction<string | undefined>>;
+    setRegex: React.Dispatch<React.SetStateAction<string | undefined>>;
+    setSeparators: React.Dispatch<React.SetStateAction<string[] | undefined>>;
+    setError: React.Dispatch<React.SetStateAction<Error | undefined>>;
 };
 
 let LineOptionsForm = (props: LineOptionsFormProps) => {
     return (
         <>
-            <Input defaultValue={props.separators}
-                   onChange={(event) => props.setSeparators(event.target.value)} />
-            <Input onChange={(event) => props.setRegex(event.target.value)} />
-            <Input onChange={(event) => props.setIndices(event.target.value)} />
+            <Input defaultValue={props.separators && props.separators.length > 0 ? props.separators[0] : ""}
+                   onChange={(event) => changeLineSeparators(props.clientId, props.separators, [event.target.value], props.setSeparators, props.setError)} />
+            <Input onChange={(event) => changeLineRegex(props.clientId, props.regex, event.target.value, props.setRegex, props.setError)} />
+            <Input onChange={(event) => changeLineIndices(props.clientId, props.indices, event.target.value, props.setIndices, props.setError)} />
         </>
     );
 };
